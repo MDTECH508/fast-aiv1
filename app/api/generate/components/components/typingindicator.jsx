5e931e94906a35467325d97dbfc9917f4a500556
MDTECH508 <goodfreefire509@gmail.com>
2026-07-24 22:20:22 -0400

    Create typingindicator.jsx

---
+++ app/api/generate/components/components/typingindicator.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function TypingIndicator() {
+  return (
+    <div className="typing-indicator">
+      <div className="dot dot1" />
+      <div className="dot dot2" />
+      <div className="dot dot3" />
+    </div>
+  );
+}
